85a19f5c0fa47be2bf23698fc818a0573b8e0e6b
sadng <115597343+sadng@users.noreply.github.com>
2022-11-01 18:59:48 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ var map = L.map('map').setView([47.25, -122.44], 11);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/navigation-day-v1', 'mapbox/navigation-night-v1',
     accessToken: 'pk.eyJ1Ijoic3FuZ3V5ZW4iLCJhIjoiY2w5eXd1YXc0MDk3MjNucDg2cDhyN3JrbyJ9.aMzoD2AZBPUtaVP2yV5N-A',
 }).addTo(map);
