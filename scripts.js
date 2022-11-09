f7621021abff26a20c64b1787d7ef63ae919a772
sadng <115597343+sadng@users.noreply.github.com>
2022-11-08 19:11:26 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,3 @@
-var directions = [
-  "Welcome to the map!\nTo get started, click on the map and it'll add your first waypoint!\nContinue to click places on the map to add additional waypoints!!"
-];
-window.alert(directions);
-document.getElementById("demo").innerHTML = directions;
-
 var map = L.map('map').setView([47.25, -122.44], 11);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,35 +5,34 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_
     maxZoom: 18,
     id: 'mapbox/navigation-day-v1',
     accessToken: 'pk.eyJ1Ijoic3FuZ3V5ZW4iLCJhIjoiY2w5eXd1YXc0MDk3MjNucDg2cDhyN3JrbyJ9.aMzoD2AZBPUtaVP2yV5N-A',
-}).addTo(map);
+	}).addTo(map);
 
-      var control = L.Routing.control({
-          waypoints: [
-				null
-              //L.latLng(47.246587, -122.438830),
-			  //L.latLng(47.258024, -122.444725),
-              //L.latLng(47.318017, -122.542970)
-          ],
-           routeWhileDragging: true
-		   router: L.Routing.mapbox('pk.eyJ1Ijoic3FuZ3V5ZW4iLCJhIjoiY2w5eXd1YXc0MDk3MjNucDg2cDhyN3JrbyJ9.aMzoD2AZBPUtaVP2yV5N-A'),
+var control = L.Routing.control({
+	waypoints: [
+		null
+		//L.latLng(47.246587, -122.438830),
+		//L.latLng(47.258024, -122.444725),
+		//L.latLng(47.318017, -122.542970)
+	],
+	routeWhileDragging: true,
+	router: L.Routing.mapbox('pk.eyJ1Ijoic3FuZ3V5ZW4iLCJhIjoiY2w5eXd1YXc0MDk3MjNucDg2cDhyN3JrbyJ9.aMzoD2AZBPUtaVP2yV5N-A'),
 			units:'imperial',
 			collapsible: true,
-			  geocoder: L.Control.Geocoder.photon(),
-	  }).addTo(map);
-	  
-        <!-- hide to hide the map upon opening -->
+			show: false,
+			geocoder: L.Control.Geocoder.photon(),
+	}).addTo(map);
 		
-		function createButton(label, container) {
-    var btn = L.DomUtil.create('button', '', container);
-    btn.setAttribute('type', 'button');
-    btn.innerHTML = label;
+function createButton(label, container) {
+	var btn = L.DomUtil.create('button', '', container);
+		btn.setAttribute('type', 'button');
+		btn.innerHTML = label;
     return btn;
 }
 
 map.on('click', function(e) {
     var container = L.DomUtil.create('div'),
-        startBtn = createButton('Start from this location', container),
-        destBtn = createButton('Go to this location', container);
+		startBtn = createButton('Start from this location', container),
+		destBtn = createButton('Go to this location', container);
 
     L.popup()
         .setContent(container)
@@ -56,3 +49,9 @@ map.on('click', function(e) {
         map.closePopup();
     });
  });
+ 
+ var directions = [
+	"Welcome to the map!\nTo get started, click on the map and add your first waypoint by selecting 'Start from this location'!\nSelect another location and click 'Go to this location' to create your second waypoint!!\nAdd additional waypoints by selecting the + in the top right routing box."
+	];
+	window.alert(directions);
+	document.getElementById("demo").innerHTML = directions;
